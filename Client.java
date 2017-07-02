c8015fdf1513a4b97e829ebaf5573e6dab09cf3f
MariamaB <candy91.mb@googlemail.com>
2017-07-02 13:16:21 +0200

    add openCsv.jar and refresh repo

--- Client.java
+++ Client.java
@@ -15,8 +15,13 @@ public class Client {
 		bMA.createKonto(123,"Nan Danke", "Felix", "DE84 1005 0000 6012 9878 24", 
 				4321, 50.000);
 		
+		BankMitarbeiter bMA3 = new BankMitarbeiter();
+		bMA.createKonto(123,"Mustermann", "Will", "DE82 1005 0000 6511 3338 77", 
+				5678, 90.000);
+		
 		BankAutomatProxy bA = new BankAutomatProxy("DE82 1005 0000 6011 9838 88");
 		BankAutomatProxy bA2 = new BankAutomatProxy("DE84 1005 0000 6012 9878 24");
+		
 		System.out.println("MIriam: "+bA.getKontostand()+"  Felix: "+bA2.getKontostand());
 		
 		
--- dismissed/klasse2.java
+++
@@ -1,5 +0,0 @@
-package htw.designpattern.projekt.proxy.dismissed;
-
-public class klasse2 extends klasse1{
-
-}
